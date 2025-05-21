06f7909b8c153ddd7c21a153a73e1feee5a32028
LahiruMadhawaWork <lahirumadhawawork@gmail.com>
2025-05-21 14:59:30 +0530

    first commit

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,74 @@
+package base;
+
+import java.time.Duration;
+
+import com.aventstack.chaintest.plugins.ChainTestListener;
+import com.aventstack.chaintest.service.ChainPluginService;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @Parameters({"url", "browser"})
+    @BeforeTest
+    public void setUp(String url, String browserName) {
+        System.out.println("browser is : " + browserName);
+        ChainPluginService.getInstance().addSystemInfo("Build#", "1.0");
+        ChainPluginService.getInstance().addSystemInfo("Owner Name#", "Naveen Khunteta");
+        switch (browserName.toLowerCase().trim()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
+                break;
+            default:
+                System.out.println("plz supply the right browser...." + browserName);
+                throw new IllegalArgumentException("WRONG BROWSER : " + browserName);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(url);
+    }
+
+    @AfterMethod
+    public void attachScreenshot(ITestResult result){
+        if(!result.isSuccess()){
+            ChainTestListener.embed(takeScreenshot(), "image/png");
+        }
+    }
+
+    /**
+     * takescreenshot
+     * @return
+     */
+
+    public byte[] takeScreenshot(){
+       return ((TakesScreenshot)(driver)).getScreenshotAs(OutputType.BYTES);
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
